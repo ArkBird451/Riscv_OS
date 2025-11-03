ffcebb1cfafd37a1d58111ebf839e9e65e33aa07
Arkbird451 <muhdtalha6@gmail.com>
2025-11-03 19:29:04 +0500

    Exception

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@ set -xue
 QEMU=qemu-system-riscv32
 
 # Path to clang and compiler flags
-CC=/opt/homebrew/opt/llvm/bin/clang  # Ubuntu users: use CC=clang
+# Allow override: CC=<path-to-clang> ./run.sh
+CC=${CC:-clang}
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    boot.S kernel.c common.c
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
